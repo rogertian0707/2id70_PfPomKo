7cab6c1790ac8def609fc348aaabccb784543eca
rogertian0707 <rogertian0707@outlook.com>
2019-02-28 02:43:36 +0100

    test push

--- q2Create.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX idx_studentRegistrationID ON CourseRegistrations(studentregistrationID);
--- Takes 3:18 minutes. Drastically improves time needed for queries 1,6 and 8 (still need to test the others).
-CREATE INDEX idx_student_degree ON studentregistrationstodegrees(studentid,degreeid);
--- Takes 16 seconds. Makes query 1 basically instant.
