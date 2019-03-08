f3ff58d46fff71b3a6b766f7239248fb4be0ddc3
rogertian88 <z.tian@student.tue.nl>
2019-03-08 23:03:27 +0100

    add q2 q3

--- q2Create.sql
+++ q2Create.sql
@@ -25,6 +25,35 @@ WHERE cr.CourseOfferId = co.CourseOfferId
  	AND co.Year = 2018 AND co.Quartile = 1
 GROUP BY cr.CourseOfferId;
 
+CREATE OR REPLACE VIEW failed_UNION_4 AS
+SELECT studentregistrationid FROM courseregistrations_4 UNION ALL SELECT studentregistrationid FROM courseregistrations_failed
+;
+
+CREATE VIEW active AS
+SELECT s.studentid, studentname, address, birthyearstudent, gender
+FROM    (SELECT cr.studentregistrationID, SUM(c.ects)
+	FROM courseregistrations_passed as cr, courses as c, courseoffers as co
+	WHERE cr.courseofferid = co.courseofferid AND co.courseid = c.courseid
+	GROUP BY cr.studentregistrationID) as currentECTS(studentregistrationID, ECTS), 
+	(SELECT studentID, srtd.studentregistrationID, totalects
+	FROM degrees as d, studentregistrationstodegrees as srtd
+	WHERE d.degreeid = srtd.degreeid) as neededECTS(studentID,studentregistrationID, ECTS),
+	students as s
+WHERE	currentECTS.studentregistrationID = neededECTS.studentregistrationID AND
+	currentECTS.ECTS < neededECTS.ECTS AND
+	neededECTS.studentid = s.studentid
+UNION ALL
+SELECT 	s.studentid, studentname, address, birthyearstudent, gender
+FROM 	(SELECT studentregistrationid
+	FROM studentregistrationstodegrees
+	EXCEPT
+	SELECT DISTINCT studentregistrationid
+	FROM Courseregistrations) as noCourses,
+	students as s, studentregistrationstodegrees as srtd
+WHERE	s.studentid = srtd.studentid AND
+	srtd.studentregistrationid = noCourses.studentregistrationid
+;
+
 --MATERIALIZED VIEW
 CREATE MATERIALIZED VIEW good_student(sid,good_grades) AS
 SELECT srtg.StudentId AS sid , cr.grade AS good_grades
@@ -32,6 +61,31 @@ FROM max_grade_for_coid, CourseRegistrations_passed AS cr, StudentRegistrationsT
 WHERE srtg.StudentRegistrationId = cr.StudentRegistrationId
 	AND max_grade_for_coid.coid  = cr.CourseOfferId
 	AND max_grade_for_coid.max_grade = cr.Grade
+;
+
+CREATE MATERIALIZED VIEW non_fail_stu AS
+SELECT *
+FROM courseregistrations_passed
+WHERE NOT EXISTS( SELECT failed_UNION_4.studentregistrationid FROM failed_UNION_4 WHERE failed_UNION_4.studentregistrationid = courseregistrations_passed.studentregistrationid)
+;
+
+CREATE MATERIALIZED VIEW GPA AS
+SELECT
+	sr_to_deg.studentregistrationid as srid,
+	(SUM(cr_passed.grade*courses.ects)/SUM(courses.ects)) AS GPA_score,
+	(CASE WHEN SUM(courses.ects)<degrees.totalects THEN 1 ELSE 0 END) AS degree_complete_or_not
+FROM
+	studentregistrationstodegrees AS sr_to_deg,
+	courseregistrations_passed AS cr_passed,
+	courseoffers AS co,
+	courses,
+	degrees
+WHERE
+		courses.courseid = co.courseid
+	AND co.courseofferid = cr_passed.courseofferid
+	AND sr_to_deg.studentregistrationid = cr_passed.studentregistrationid
+	AND sr_to_deg.degreeid = degrees.degreeid
+GROUP BY sr_to_deg.studentregistrationid,degrees.totalects
 ;
 
 --INDICES
